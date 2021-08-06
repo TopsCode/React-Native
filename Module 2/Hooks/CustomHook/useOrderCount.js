b394d446445639678278f31db8b8c1c2c6cdf182
TopsCode <kailash.solanki@tops-int.com>
2021-08-06 13:29:34 +0530

    Extra Files Removed

---
+++ Module 2/Hooks/CustomHook/useOrderCount.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function useOrderCountHook() {
+   const [orderCount, setOrderCount] = useState({ count: 0 });
+   
+   const changeOrderCount = () => {
+      setOrderCount({ count: orderCount.count + 1 }) 
+    }
+   return { orderCount, changeOrderCount };
+}
+export default useOrderCountHook;
