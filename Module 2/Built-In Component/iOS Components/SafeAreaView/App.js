2be25b62e482062bde820915b6d40d794973c6a4
TopsCode <kailash.solanki@tops-int.com>
2021-08-06 14:20:33 +0530

    Extra Folder Removed

---
+++ Module 2/Built-In Component/iOS Components/SafeAreaView/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { ActionSheetIOS, Button, StyleSheet, Text, View } from "react-native";
+
+import React from 'react';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Page content</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
